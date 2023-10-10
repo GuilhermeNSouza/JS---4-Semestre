bb034ce75722780c857c490ded49270232d122d5
GuilhermeNSouza <guilhermenariodesouza@gmail.com>
2023-10-09 21:45:03 -0400

    nova versão

--- Aulas (ATV)/02-10-2023/server.js
+++ Aulas (ATV)/02-10-2023/server.js
@@ -1,11 +1,17 @@
-const express = require ('express');
-const app = express ();
+const express = require('express')
+const app = express()
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.set('view engine', 'ejs');
-app.get("/", function(request, response) {
+app.use('/css', 
+express.static(__dirname + 'public/css'))
+app.set('view engine','ejs')
+app.get("/", function(request, response){
     response.render('pages/index')
 })
-
-app.listen(8080);
-console.log('Tá conectando');
+app.get("/sobre", function(request, response){
+    response.render('pages/about')
+})
+app.get("/contato", function(request, response){
+    response.render('pages/contato')
+})
+app.listen(8080)
+console.log('Tá carregando');
